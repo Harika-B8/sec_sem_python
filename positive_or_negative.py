f71201e63e0cdbe915eb176a615b8e8b8aebce43
Harika-B8 <120396657+Harika-B8@users.noreply.github.com>
2023-04-26 15:51:30 +0530

    positive_or_negative.py

---
+++ positive_or_negative.py
@@ -0,0 +1,7 @@
+num=int(input("enter the number: ")
+        if num < 0:
+      print("the num is negative",num)
+   elif:
+       print("the num is positive",num)
+else:
+    print(" the num is not numeric",num)
