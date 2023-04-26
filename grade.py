61180299d70f51dd98df276b1f7a75f01691355c
Harika-B8 <120396657+Harika-B8@users.noreply.github.com>
2023-04-26 15:30:42 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,13 @@
+
+marks=int(input("enter the marks: "))
+if marks>75:
+    print('O grade')
+elif marks>60 and marks<75:
+    print('A grade')
+elif marks>50 and marks<60:
+    print('B grade')
+elif marks>35 and marks<50:
+    print('C grade')
+else:
+    print('F grade')
+
