1a1882fef424b7e31091ee478b23a0ca0dd007ab
Harika-B8 <120396657+Harika-B8@users.noreply.github.com>
2023-04-26 15:27:26 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+
+num=int(input("enter the num: "))
+a=num
+num1=0
+while(a>0):
+    r=a%10
+    num1=num1*10+r
+    a=a//10
+if(num==num1):
+    print("palindrome")
+else:
+    print("not a palindrome")
+
