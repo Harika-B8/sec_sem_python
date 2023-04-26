3fe00af806a64f83de5885bbfa940bce8b429577
Harika-B8 <120396657+Harika-B8@users.noreply.github.com>
2023-04-26 15:26:12 +0530

    limit.py

---
+++ limit.py
@@ -0,0 +1,9 @@
+
+num=int(input("enter the num: "))
+l_limit=int(input("enter the num: "))
+u_limit=int(input("enter the num: "))
+if num in range(l_limit,u_limit):
+    print("in range!!")
+else:
+    print("out of range!!")
+
